fe962f3677afb75be167e71dc865163c7705f1ea
multicampus <blessisu@naver.com>
2021-07-26 05:04:02 +0900

    feat.sweet-list UI

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@
     <div class="flex-1">
       <div class="flex h-screen">
         <!-- main section -->
-        <div class="flex-1 border-r bodrer-gray-100">
+        <div class="flex-1 border-r bodrer-gray-100 overflow-y-auto">
           <div class="flex flex-col">
             <!-- main title -->
             <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">홈</div>
@@ -86,6 +86,46 @@
                   <button class="bg-primary text-white text-sm font-bold px-4 py-1 rounded-full hover:bg-dark">
                     Sweet
                   </button>
+                </div>
+              </div>
+            </div>
+            <!-- main sweet list -->
+            <div 
+              class="flex px-3 py-3 border-b border-gray-100" 
+              v-for="sweet in 10"
+              :key="sweet"
+            >
+              <img src="http://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-70 cursor-pointer">
+              <div class="ml-3 flex-1 flex flex-col space-y-1">
+                <!-- sweet list head -->
+                <div class="text-sm space-x-1">
+                  <span class="font-bold">수현</span>
+                  <span class="text-gray-500 text-xs">@blessisu</span>
+                  <span>·</span>
+                  <span class="text-gray-500 text-xs">3분</span>
+                </div>
+                <!-- sweet list body -->
+                <div>
+                  공부는 새벽해야 제 맛이다..
+                </div>
+                <!-- sweet list icons -->
+                <div class="flex justify-between">
+                  <div class="text-gray-500 hover:text-blue-500">
+                    <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
+                    <span class="ml-1 text-sm">10</span>
+                  </div>
+                  <div class="text-gray-500 hover:text-green-500">
+                    <i class="fas fa-retweet hover:bg-green-50 rounded-full p-2"></i>
+                    <span class="ml-1 text-sm">10</span>
+                  </div>
+                  <div class="text-gray-500 hover:text-pink-500">
+                    <i class="far fa-heart hover:bg-pink-50 rounded-full p-2"></i>
+                    <span class="ml-1 text-sm">10</span>
+                  </div>
+                  <div class="text-gray-500 hover:text-blue-500">
+                    <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
+                    <span class="ml-1 text-sm">10</span>
+                  </div>
                 </div>
               </div>
             </div>
